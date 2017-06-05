827a2fe5b626ad98e643f7097af69d65a81345b6
dbeckdorf <dbeckdorf@miuandes.cl>
2017-06-05 15:58:16 -0400

    2 commit:     test_record_roll__with_new_game_and_5_pins_knocked__game_get_score_return_cero

--- tests/tests_game.py
+++ tests/tests_game.py
@@ -4,11 +4,20 @@ from app import game
 
 
 class GameTest(unittest.TestCase):
-    def test_record_roll__with_new_game_and_cero_num_pins_knocked__game_return_cero(self):
+    def test_record_roll__with_new_game_and_cero_num_pins_knocked__game_get_score_return_cero(self):
         # Assert
         bowling_game = game.Game()
         # Act
         bowling_game.record_roll(num_pins_knocked=0)
+        result = bowling_game.get_score()
+        # Assert
+        self.assertEqual(first=0, second=result)
+
+    def test_record_roll__with_new_game_and_5_pins_knocked__game_get_score_return_cero(self):
+        # Assert
+        bowling_game = game.Game()
+        # Act
+        bowling_game.record_roll(num_pins_knocked=5)
         result = bowling_game.get_score()
         # Assert
         self.assertEqual(first=0, second=result)

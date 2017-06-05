3a2c3b9d48027f3d36dc267eae7697e0f5e344d1
dbeckdorf <dbeckdorf@miuandes.cl>
2017-06-05 16:14:30 -0400

    4 commit:     test_record_roll__5_pins_knocked_after_3_completed_sets_with_4_4_6_1_0_5__game_get_score_return_20

--- tests/tests_game.py
+++ tests/tests_game.py
@@ -22,7 +22,7 @@ class GameTest(unittest.TestCase):
         # Assert
         self.assertEqual(first=0, second=result)
 
-    def test_record_roll__with_new_game_and_5_pins_knocked_after_0_pins_knocked__game_get_score_return_5(self):
+    def test_record_roll__5_pins_knocked_after_0_pins_knocked__game_get_score_return_5(self):
         # Assert
         bowling_game = game.Game()
         bowling_game.record_roll(num_pins_knocked=0)
@@ -31,3 +31,18 @@ class GameTest(unittest.TestCase):
         result = bowling_game.get_score()
         # Assert
         self.assertEqual(first=5, second=result)
+
+    def test_record_roll__5_pins_knocked_after_3_completed_sets_with_4_4_6_1_0_5__game_get_score_return_20(self):
+        # Assert
+        bowling_game = game.Game()
+        bowling_game.record_roll(num_pins_knocked=4)
+        bowling_game.record_roll(num_pins_knocked=4)
+        bowling_game.record_roll(num_pins_knocked=6)
+        bowling_game.record_roll(num_pins_knocked=1)
+        bowling_game.record_roll(num_pins_knocked=0)
+        bowling_game.record_roll(num_pins_knocked=5)
+        # Act
+        bowling_game.record_roll(num_pins_knocked=5)
+        result = bowling_game.get_score()
+        # Assert
+        self.assertEqual(first=20, second=result)
